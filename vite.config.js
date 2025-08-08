2d06c4a8f9518432f7cd339a17f6d72efe20e703
Tabrezhira <Tabrez.hira@gmail.com>
2025-08-08 15:58:37 +0530

    test 3

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,22 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  // ❌ Removed assetsInclude for .html because it breaks index.html parsing
+  plugins: [
+    react(),
+    tailwindcss(),
+    visualizer({
+      filename: './dist/stats.html', // output file path
+      open: true,                    // auto open in browser after build
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // @ → /src
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
