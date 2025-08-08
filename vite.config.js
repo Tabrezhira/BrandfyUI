daa962f96131d2e9da09ab77b87a5e0e4f5f8e4d
Tabrezhira <Tabrez.hira@gmail.com>
2025-08-08 16:39:39 +0530

    done4

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,9 @@ export default defineConfig({
     react(),
     tailwindcss(),
     visualizer({
-      filename: './dist/stats.html', // output file path
-      open: true,                    // auto open in browser after build
+      template:'treemap',
+      filename: './dist/stats.html', // Output file path for the bundle visualize report
+      open: true,                    // Auto-open in browser after build
       gzipSize: true,
       brotliSize: true,
     }),
@@ -18,6 +19,18 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Group each package into its own chunk
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
     },
   },
 });
